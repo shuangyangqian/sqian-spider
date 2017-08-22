3ab150725e5d204daaa0e1def5e1f01e2cb10d28
sqian <shuangyang.qian@easystack.cn>
2017-08-22 23:05:36 +0800

    Add the folder sqian_requests
    Add the folder sqian_requests which will
    use the requests to write some functions,
    and write some simple function in the
    sqian_requests/main.py.

---
+++ sqian_requests/main.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+import requests
+import json
+
+URL = "https://api.github.com"
+
+
+def build_uri(endpoint):
+    return "/".join([URL,endpoint])
+
+
+def better_print(json_str):
+    return json.dumps(json.loads(json_str), indent=4)
+
+
+def simple_requests():
+    response = requests.get(build_uri('users/guojingyu'))
+    print better_print(response.text)
+
+
+simple_requests()
